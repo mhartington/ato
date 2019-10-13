ae1d9d279d5e85b153eeae5f21af5f1437db7662
Mike Hartington <mikehartington@gmail.com>
2019-10-13 10:17:49 -0400

    how i hate gatsby

--- slides/gatsby-config.js
+++ slides/gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
+  pathPrefix: '/ato',
   plugins: [
+    'gatsby-plugin-catch-links',
     {
       resolve: 'gatsby-theme-mdx-deck',
       options: {
-        basePath: '/ato'
-      }
-    }
-  ]
+        basePath: '/',
+      },
+    },
+  ],
 }
